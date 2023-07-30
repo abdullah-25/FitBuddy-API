dcec3e66cc699ea27a462968950ddb8957ccdfef
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-07-30 00:04:44 -0400

    back end routes for maxweight and exercised are working fully with all edge cases

--- routes/exercises.js
+++ routes/exercises.js
@@ -13,34 +13,65 @@ function getExercises(req, res) {
 }
 
 function postExercise(req, res) {
-  const { users_id, exercise_names } = req.body;
-  console.log(req.body);
-
-  if (!Array.isArray(exercise_names)) {
-    return res.status(400).send("exercise_names should be an array");
-  }
-
-  const exercisesToInsert = exercise_names.map((exercise_name) => ({
-    users_id,
-    exercise_name,
-  }));
-  console.log(exercisesToInsert);
+  const { users_id, exercise_name } = req.body;
 
   knex("exercises")
-    .insert(exercisesToInsert)
-    .onConflict("exercise_name")
-    .ignore() // This will skip insert if exercise_name already exists
-    .then(() => {
-      res.status(200).send("Exercises created successfully");
+    .where({ users_id, exercise_name })
+    .first()
+    .then((existingExercise) => {
+      if (existingExercise) {
+        return res
+          .status(409)
+          .json({ message: `Exercise name already exists for the user` });
+      }
+
+      const newEntry = { users_id, exercise_name };
+
+      knex("exercises")
+        .insert(newEntry)
+        .then(() => {
+          res.status(200).send("Exercise created successfully");
+        })
+        .catch((error) => {
+          res
+            .status(500)
+            .json({ message: "Error inserting exercise: " + error.message });
+        });
     })
     .catch((error) => {
-      res.status(500).send("Error inserting exercises: " + error.message);
+      res.status(500).json({
+        message: `Error checking for duplicate exercise: ${error.message}`,
+      });
     });
 }
 
-// Check if an exercise with the same name already exists in the database
+function findExerciseId(req, res) {
+  const { users_id, exercise_name } = req.body;
+
+  // Find the exercise_id associated with the provided exercise_name and users_id
+  knex("exercises")
+    .select("id")
+    .where({ users_id, exercise_name })
+    .first()
+    .then((exercise) => {
+      if (!exercise) {
+        res.status(404).send("Exercise not found for the given user and name.");
+      } else {
+        const result = {
+          users_id: users_id,
+          exercises_id: exercise.id,
+          exercise_name: exercise_name,
+        };
+        res.status(200).json(result);
+      }
+    })
+    .catch((error) => {
+      res.status(500).send("Error finding exercise: " + error.message);
+    });
+}
 
 router.get("/", getExercises);
 router.post("/", postExercise);
+router.get("/id", findExerciseId);
 
 module.exports = router;
--- routes/maxweight.js
+++ routes/maxweight.js
@@ -12,39 +12,19 @@ function getMaxWeight(req, res) {
     });
 }
 
-function postMaxWeight(req, res) {
-  const { users_id, exercise_name, max_weight } = req.body;
-  console.log(req.body);
+function updateMaxWeight(req, res) {
+  const { exercises_id, max_weight } = req.body;
 
-  // Check if an exercise with the same name already exists in the database
-  knex("maxweight")
-    .select("exercises_id")
-    .where("exercises_id", exercises_id)
-    .first()
-    .then((existingExercise) => {
-      if (existingExercise) {
-        // Exercise with the same name already exists, do nothing
-        const newMaxWeight = {
-          max_weight: max_weight,
-        };
-        knex("maxweight")
-          .insert(newMaxWeight)
-          .then(() => {
-            res.status(200).send("Max Weight updated successfully");
-          })
-          .catch((error) => {
-            res
-              .status(500)
-              .send("Error inserting Max Weight: " + error.message);
-          });
-      }
-    })
-    .catch((error) => {
-      res.status(500).send("Error checking exercise: " + error.message);
+  const addWeightEntry = { exercises_id, max_weight };
+
+  knex("max_weight")
+    .insert(addWeightEntry)
+    .then(() => {
+      res.status(200).send("Weight Added successfully");
     });
 }
 
 router.get("/", getMaxWeight);
-router.post("/", postMaxWeight);
+router.post("/", updateMaxWeight);
 
 module.exports = router;
